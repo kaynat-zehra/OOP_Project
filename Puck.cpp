9382faa9ceb8f22364e7237106b0d905e0bf8b23
Ayeshaeiman <ayesha.eiman.r@gmail.com>
2023-11-17 10:01:18 +0500

    d

--- Puck.cpp
+++ Puck.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include "Puck.hpp"
+
+void Puck::set_coordinates(int x,int y){
+    puck.x_coordinate=x;
+    puck.y_coordinate=y;
+}
+
+void Puck::set_velocity(double v){
+    velocity=v;
+}
--- Puck_struct.hpp
+++ Puck_struct.hpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
 struct Puck_struct{
-    int get_coordinates(){
-        return (x_coordinate,y_coordinate);
-    }
-    int get_radius(){
-        return radius;
-    }
-    private:
     int x_coordinate;
     int y_coordinate;
     int radius{36};
