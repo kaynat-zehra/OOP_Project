4c6f8bef01fce923f6d294a247b2ac1dc75e5f7e
Ayeshaeiman <ayesha.eiman.r@gmail.com>
2023-11-17 09:55:31 +0500

    up

--- Puck.hpp
+++ Puck.hpp
@@ -4,6 +4,9 @@
 class Puck{
     public:
     void set_coordinates(int x,int y);
+    void reset_puck();
+    void set_velocity(double v);
+    bool is_colliding();
     private:
     Puck_struct puck;
     double velocity;
