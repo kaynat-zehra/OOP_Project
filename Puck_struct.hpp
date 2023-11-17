0dd7e223be5beacd9164517a4157727ee0371139
Ayeshaeiman <ayesha.eiman.r@gmail.com>
2023-11-17 09:43:21 +0500

    Update Puck_struct.hpp

--- Puck_struct.hpp
+++ Puck_struct.hpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+struct Puck_struct{
+    double get_coordinates(){
+        return (x_coordinate,y_coordinate);
+    }
+    double get_radius(){
+        return radius;
+    }
+    private:
+    double x_coordinate;
+    double y_coordinate;
+    double radius;
+};
