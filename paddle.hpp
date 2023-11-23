2bf5bb80f7b4c14025ccc548ba871c118f0778df
Ayeshaeiman <ayesha.eiman.r@gmail.com>
2023-11-23 11:48:05 +0500

    paddle update

--- paddle.hpp
+++ paddle.hpp
@@ -1,4 +1,26 @@
 #include <iostream>
+
 class Paddle{
-    
+    protected:
+    float x, y;
+    float width, height;
+    int speed;
+    void LimitMovement() {
+        if (y <= 0) {
+            y = 0;
+        }
+        if (y + height >= 600) {
+            y = 600 - height;
+        }
+    }
+    public:
+    void Update() {
+        if (IsKeyDown(KEY_UP)) {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
 };
