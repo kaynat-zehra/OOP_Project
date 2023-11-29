49903fc4400bcfc78df4b817c3d2d91bab1a6cd0
rameen08462 <122247568+rameen08462@users.noreply.github.com>
2023-11-29 15:10:31 +0500

    Update Puck.cpp

--- Puck.cpp
+++ Puck.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
-#include "Puck.hpp"
+// #include <iostream>
+// #include "Puck.hpp"
 
-void Puck::set_coordinates(int x,int y){
-    puck.x_coordinate=x;
-    puck.y_coordinate=y;
-}
+// void Puck::Update(){
+//     puck.x_coordinate+=velocity_x;
+//     puck.y_coordinate+=velocity_y;
+//     if (puck.y_coordinate>=599 || puck.y_coordinate<=0){
+//         velocity_y*=-1;
+//     }
+//     // SDL_RenderCopy(gRenderer, assets, &puck.srcRect, &puck.moverRect);
 
-void Puck::set_velocity(double v){
-    velocity=v;
-}
+// }
+// void Puck::set_coordinates(int x,int y){
+//     puck.x_coordinate=x;
+//     puck.y_coordinate=y;
+// }
+
+// void Puck::set_velocity(double v){
+//     velocity_x=v;
+//     velocity_y=v;
+// }
