fb5f0b793ec46aceac95202d4f0ed0b81d51f2a6
rameen08462 <122247568+rameen08462@users.noreply.github.com>
2023-11-29 15:10:43 +0500

    Update Puck.hpp

--- Puck.hpp
+++ Puck.hpp
@@ -3,11 +3,14 @@
 
 class Puck{
     public:
+    Puck(){};
+    void Update();
     void set_coordinates(int x,int y);
     void reset_puck();
     void set_velocity(double v);
     bool is_colliding();
     private:
     Puck_struct puck;
-    double velocity;
+    double velocity_x;
+    double velocity_y;
 };
