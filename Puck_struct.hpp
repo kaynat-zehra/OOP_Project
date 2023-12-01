65d99bc4fcad9cf4da9c44601fb9a8c9b3994dd8
rameen08462 <122247568+rameen08462@users.noreply.github.com>
2023-12-01 09:42:49 +0500

    update

--- Puck_struct.hpp
+++ Puck_struct.hpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "Unit.hpp"
+// #include "SDL.h"
 
 struct Puck_struct{
-    int x_coordinate;
-    int y_coordinate;
-    int radius{36};
-    Puck_struct():x_coordinate(506-82),y_coordinate(293-60){};
+    int x_coordinate=72;
+    int y_coordinate=72;
+    int puck_w=119;
+    int puck_h=101;
+    // int radius{36};
+    // Puck_struct(){
+    //     Unit puck;
+    //     srcRect={72,72,119,101};
+    //     moverRect{506-82,293-60,70,50};
+
+    // };
 };
--- Unit.cpp
+++ Unit.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+#include "unit.hpp"
+
+Unit:: Unit() {};
+
+
+
+//fully implemented draw function
+void Unit::draw(SDL_Renderer* gRenderer, SDL_Texture* assets)
+{
+    SDL_RenderCopy(gRenderer, assets, &srcRect, &moverRect);
+}
+
