269dfc563c488d31e7ce44aef778c6bf408ef781
rameen08462 <122247568+rameen08462@users.noreply.github.com>
2023-12-01 09:42:06 +0500

    Update Unit.hpp

--- Unit.hpp
+++ Unit.hpp
@@ -1,1 +1,24 @@
 #include <iostream>
+#include "SDL.h"
+// #include "drawing.hpp"
+#pragma once
+// class Drawing{
+//     public:
+//         //The window renderer
+//     static SDL_Renderer* gRenderer;
+//     //global reference to png image sheets
+//     static SDL_Texture* assets;
+// };
+class Unit
+{
+    public:
+    int frame;
+    SDL_Rect srcRect, moverRect;
+        static SDL_Renderer* gRenderer;
+    static SDL_Texture* assets;
+
+    public:
+    Unit(); //constructor
+    void draw(SDL_Renderer* gRenderer, SDL_Texture* assets); 
+    
+};
